1dd027980fb2ac0f97b44bc5517cf7a1052c9bc8
ReplayerXZ <153446308+ReplayerXZ@users.noreply.github.com>
2024-01-05 02:22:27 +0700

    Add files via upload

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import {RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiPinterestLine, RiBehanceLine } from 'react-icons/ri'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-blue-400 transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={''} className="hover:text-blue-400 transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={''} className="hover:text-blue-400 transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={''} className="hover:text-blue-400 transition-all duration-300">
+        <RiDribbbleLine />
+      </Link>
+      <Link href={''} className="hover:text-blue-400 transition-all duration-300">
+        <RiPinterestLine />
+      </Link>
+      <Link href={''} className="hover:text-blue-400 transition-all duration-300">
+        <RiBehanceLine />
+      </Link>
+    </div>
+  )
 };
 
 export default Socials;
